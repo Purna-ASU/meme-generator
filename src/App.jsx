f2b8e0bd2714fc97a67a141d78f686487689d9d8
Purna-ASU <ppeddire@asu.edu>
2023-08-09 13:29:21 -0700

    random url

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 
-import './App.css'
-import Form from './components/Form'
-import Header from './components/Header'
+import './App.css';
+import Form from './components/Form';
+import Header from './components/Header';
+import MemeData from './assets/memesData';
 
 function App() {
+
+//   let memes = MemeData.map( items => {
+//     return <Form 
+//       key = {items.data.memes.id}
+//       {...items}
+//     />
+//   }
+// )
 
   return (
     <div>
       <Header />
+      {/* {memes} */}
       <Form />
     </div>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,20 @@
-function Form() {
+import memesData from "../assets/memesData";
+
+function Form(props) {
+
+    function clickHandler() {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url;
+    }
+
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" className="form--input" placeholder="Top Text"/>
                 <input type="text" className="form--input" placeholder="Bottom Text"/>
-                <button  className="form--button">Get a new meme image 🖼</button>
-            </form>
+                <button onClick={clickHandler} className="form--button">Get a new meme image 🖼</button>
+            </div>
         </main>
     )
 };
